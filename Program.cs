2f45c59c422fbbdd531d7a92a01f867c45646e7f
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 21:25:53 -0300

    subida al repositorio

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Hacer un programa que solicite 20 edades y luego calcule el promedio de edad de aquellas personas mayores a 18 años. */
+            
+            int acu = 0, con = 0, resultado, n;
+            for (int i = 0; i < 20; i++)
+            {
+             Console.WriteLine("Ingrese una edad"); 
+             n = int.Parse(Console.ReadLine());
+             if(n > 18){
+                con ++;
+                acu += n;
+                }  
+            }
+            resultado = acu / con; 
+            Console.WriteLine("El promedio es " + resultado);
+        }
+    }
+}
